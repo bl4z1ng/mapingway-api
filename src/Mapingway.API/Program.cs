c26091e1573200b80ac9b1411ff0d62e2a614e18
bl4z1ng <bl4z1ng>
2023-05-16 20:05:32 +0300

    feat(config): added custom file logger

--- src/Mapingway.API/Program.cs
+++ src/Mapingway.API/Program.cs
@@ -1,8 +1,12 @@
+using Mapingway.API.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Logging.AddFileLogger(Path.Combine(Directory.GetCurrentDirectory(), "log.txt"));
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
