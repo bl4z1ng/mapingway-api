8bb11fed79770067490ddfe58e0fa1160c5cc3ed
bl4z1ng <bl4z1ng>
2023-06-10 15:46:22 +0300

    feature(repository): added base interface for generic repo and user implementation.

---
+++ src/Mapingway.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,57 @@
+﻿using Mapingway.Domain.User;
+using Mapingway.Infrastructure.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mapingway.Infrastructure.Repositories;
+
+public class UserRepository : IRepository<User>
+{
+    private readonly ApplicationDbContext _context;
+
+    public UserRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<User>> GetAllAsync(CancellationToken ct = default)
+    {
+        return await _context.Users.ToListAsync(ct);
+    }
+
+    public async Task<User?> GetByIdAsync(int id, CancellationToken ct = default)
+    {
+        return await _context.Users.FindAsync(id, ct);
+    }
+
+    public async Task<List<User>> GetByConditionAsync(Func<User,bool> condition, CancellationToken ct = default)
+    {
+        return await _context.Users.Include(ent => condition).ToListAsync(ct);
+    }
+
+    public async Task<int> CreateAsync(User user, CancellationToken cancellationToken = default)
+    {
+        await _context.Users.AddAsync(user, cancellationToken: cancellationToken);
+        _context.Entry(user).State = EntityState.Added;
+
+        return user.Id;
+    }
+
+    public async Task<bool> DeleteAsync(int userId, CancellationToken ct = default)
+    {
+        var user = await _context.Users.FindAsync(userId, ct);
+        if (user is null) return false;
+
+        _context.Entry(user).State = EntityState.Deleted;
+        return true;
+    }
+
+    public void UpdateAsync(User user, CancellationToken ct = default)
+    {
+        _context.Entry(user).State = EntityState.Modified;
+    }
+
+    public async Task SaveAsync(CancellationToken cancellationToken)
+    {
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+}
