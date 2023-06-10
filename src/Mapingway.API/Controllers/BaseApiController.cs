d7c4c6a98f4ae8995ac131684160b8d41d991cb8
bl4z1ng <bl4z1ng>
2023-06-10 15:32:19 +0300

    feature(base): added base api controller and user controller.

---
+++ src/Mapingway.API/Controllers/BaseApiController.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection.Emit;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mapingway.API.Controllers;
+
+[ApiController]
+public class BaseApiController : ControllerBase
+{
+    private readonly ILogger _logger;
+    private readonly IMediator _mediator;
+
+    public BaseApiController(ILoggerFactory loggerFactory, IMediator mediator, string controllerType = "BaseController")
+    {
+        _logger = loggerFactory.CreateLogger(controllerType);
+        _mediator = mediator;
+    }
+}
---
+++ src/Mapingway.API/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mapingway.API.Controllers;
+
+[Route("User")]
+public class UserController : BaseApiController
+{
+    public UserController(ILoggerFactory loggerFactory, IMediator mediator) : base(loggerFactory, mediator, typeof(UserController).ToString())
+    {
+    }
+}
