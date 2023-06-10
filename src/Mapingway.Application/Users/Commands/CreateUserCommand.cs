d3329ab0921d0dcc330f44de6f4af7e1f701302e
bl4z1ng <bl4z1ng>
2023-06-10 16:28:39 +0300

    feature(mediatr): added first create user command.

---
+++ src/Mapingway.Application/Users/Commands/CreateUserCommand.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+
+namespace Mapingway.Application.Users.Commands;
+
+public class CreateUserCommand : IRequest
+{
+    public string? Email { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Password { get; set; }
+    public string? Role { get; set; }
+}
+
+// public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand>
+// {
+//     public CreateUserCommandHandler(IRepository<User> userRepository, ILoggerFactory loggerFactory)
+//     {
+//         
+//     }
+//     
+//     public Task Handle(CreateUserCommand request, CancellationToken cancellationToken)
+//     {
+//         throw new NotImplementedException();
+//     }
+// }
