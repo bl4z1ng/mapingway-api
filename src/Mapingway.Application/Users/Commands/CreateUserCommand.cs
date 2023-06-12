f9da90d8aaab935edd1877a8f8c978e442c73f68
bl4z1ng <bl4z1ng>
2023-06-12 15:57:49 +0300

    refactor(mediatr):

--- src/Mapingway.Application/Users/Commands/CreateUserCommand.cs
+++ src/Mapingway.Application/Users/Commands/CreateUserCommand.cs
@@ -1,6 +1,4 @@
-﻿using Mapingway.Common.Repository;
-using Mapingway.Domain.User;
-using MediatR;
+﻿using MediatR;
 
 namespace Mapingway.Application.Users.Commands;
 
@@ -11,26 +9,4 @@ public class CreateUserCommand : IRequest
     public string? LastName { get; set; }
     public string? Password { get; set; }
     public string? Role { get; set; }
-}
-
-public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand>
-{
-    private readonly IRepository<User> _usersRepository;
-
-    public CreateUserCommandHandler(IRepository<User> userRepositoryRepository)
-    {
-        _usersRepository = userRepositoryRepository;
-    }
-    
-    public async Task Handle(CreateUserCommand request, CancellationToken cancellationToken)
-    {
-        var user = new User()
-        {
-            Email = request.Email,
-            FirstName = request.FirstName,
-            LastName = request.LastName
-        };
-
-        await _usersRepository.CreateAsync(user, cancellationToken);
-    }
 }
